15a628a4535cf5ae11253fdbb255b7ec206af0e2
piers <PiersSYCh@uuzu.com>
2021-12-14 17:24:52 +0800

    added firebase environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule} from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(),
+    AppRoutingModule, 
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC7URPxhYLDBzYNazMcq0fIuAMHD9wEFxI",
+    authDomain: "projectpanda-97530.firebaseapp.com",
+    databaseURL: "https://projectpanda-97530.firebaseio.com/",
+    projectId: "projectpanda-97530",
+    storageBucket: "projectpanda-97530.appspot.com",
+    messagingSenderId: "1034733166859",
+    appId: "1:1034733166859:web:aeee87d379ed6c8606020e",
+  }
 };
 
 /*
@@ -14,3 +23,5 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+// measurementId: "G-3F44J9JRNR"
