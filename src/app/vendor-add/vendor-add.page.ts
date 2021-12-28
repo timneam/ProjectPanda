0230d2705d756a673202bceeef54869bc54e700f
Chloe <chloeom@uuzu.com>
2021-12-28 12:44:50 +0800

    btns

--- src/app/vendor-add/vendor-add.page.ts
+++ src/app/vendor-add/vendor-add.page.ts
@@ -14,4 +14,21 @@ export class VendorAddPage implements OnInit {
   backClicked() {
     this._location.back();
   }
+  item_qty = 1
+
+  incrementQty(){
+    this.item_qty += 1;
+    console.log(this.item_qty + 1);
+    }
+
+    decrementQty(){
+      if(this.item_qty-1 < 1){
+        this.item_qty = 1;
+        console.log('item_1->' + this.item_qty)
+      }
+      else{
+        this.item_qty -= 1;
+        console.log('item_2->' + this.item_qty);
+      }
+      }
 }
