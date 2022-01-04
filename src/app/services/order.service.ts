3903952736e525dd70565e1b8aea7cb7b7fd5274
unknown <timothyneam@gmail.com>
2022-01-04 22:48:36 +0800

    Fix the css for vendor side

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { collectionData } from 'rxfire/firestore';
+import { getAuth } from "firebase/auth";
+import * as firebase from '@angular/fire'
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, getFirestore, setDoc, updateDoc } from 'firebase/firestore';
+import { NavController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
+  db = getFirestore();
+
   constructor() { }
+
+    async getOrders(stallId, Orders){
+      const getOrders = await getDocs(collection(this.db, 'Stall', stallId, Orders))
+      return getOrders
+    }
+
 }
