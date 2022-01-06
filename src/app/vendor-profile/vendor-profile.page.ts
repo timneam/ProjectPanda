9b8e23a0a7c1dcbba3bfa46ec568ab54dd7e647e
unknown <timothyneam@gmail.com>
2022-01-06 22:29:31 +0800

    Logout function for vendor profile page

--- src/app/vendor-profile/vendor-profile.page.ts
+++ src/app/vendor-profile/vendor-profile.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { doc, updateDoc, deleteDoc, getFirestore, getDocs, collection, where, getDoc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vendor-profile',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendorProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private UsersService: UsersService,
+    public router: Router, 
+    public alertController: AlertController
+  ) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Log out',
+     
+      message: 'Are you sure you want to log out?',
+      buttons: [ {
+        text: 'No',
+        handler: () => {
+          alert.dismiss()
+        }
+      },
+      {
+        text: 'Logout',
+        handler: () => {
+          this.UsersService.signoutUser();
+          // this.router.navigateByUrl('/login-or-register');
+        }
+      },]
+    });
+    await alert.present();
+
+   
   }
 
 }
