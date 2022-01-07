96976d9a972e3058a8aa0f132a5a18928c2fc479
unknown <timothyneam@gmail.com>
2022-01-07 16:26:40 +0800

    Vendor Profile Page done

--- src/app/vendor-profile/vendor-profile.page.ts
+++ src/app/vendor-profile/vendor-profile.page.ts
@@ -13,13 +13,54 @@ import { Router } from '@angular/router';
 })
 export class VendorProfilePage implements OnInit {
 
+  db = getFirestore();
+  updateData: FormGroup;
+
   constructor(
     private UsersService: UsersService,
     public router: Router, 
     public alertController: AlertController
   ) { }
 
+  userData = null
+  userData2 = null
+
   ngOnInit() {
+    this.getData()
+  }
+
+  async getData() {
+    
+    //Get data from the fire auth
+    const auth = getAuth();
+    const user = auth.currentUser;
+    // console.log(user)
+    // get the data
+    const ableToGetData = await getDoc(doc(this.db, "User", user.uid))
+    
+    if (user !== null) {
+      user.providerData.forEach((profile) => {
+        // console.log("  Sign-in provider: " + profile.providerId);
+        // console.log("  Provider-specific UID: " + profile.uid);
+        // console.log("  Name: " + profile.displayName);
+        // console.log("  Email: " + profile.email);
+        // console.log("  Photo URL: " + profile.photoURL);
+        this.userData = profile
+      });
+    }
+
+    if (ableToGetData.exists) {
+      // console.log(ableToGetData.data());
+      this.userData2 = ableToGetData.data();
+    }
+    else {
+      console.log("No Such Document!");
+    }
+  }
+
+  async deleteData() {
+    const frankDocRef = doc(this.db, "User", "DeezNutz");
+    await deleteDoc(frankDocRef);
   }
 
   async presentAlert() {
@@ -45,6 +86,10 @@ export class VendorProfilePage implements OnInit {
     await alert.present();
 
    
+  }
+
+  async emailForward() {
+    this.router.navigateByUrl[('/profile-email-edit')]
   }
 
 }
