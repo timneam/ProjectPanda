3f2b4a1e3ce8ebcd710707840bc8ce2f12b94b3a
unknown <timothyneam@gmail.com>
2022-01-07 15:51:25 +0800

    Unable to get user orders

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -3,7 +3,7 @@ import { Firestore } from '@angular/fire/firestore';
 import { collectionData } from 'rxfire/firestore';
 import { getAuth } from "firebase/auth";
 import * as firebase from '@angular/fire'
-import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, getFirestore, setDoc, updateDoc } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, getFirestore, query, setDoc, updateDoc, where } from 'firebase/firestore';
 import { NavController } from '@ionic/angular';
 
 @Injectable({
@@ -14,6 +14,11 @@ export class OrderService {
   db = getFirestore();
 
   constructor() { }
+
+    async getAllStallId() {
+      const getStallId = await getDocs(collection(this.db, "Stall"))
+      return getStallId
+    }
 
     async getOrders(stallId, Orders){
       const getOrders = await getDocs(collection(this.db, 'Stall', stallId, Orders))
@@ -34,16 +39,25 @@ export class OrderService {
       const incomingOrders = await getDocs(collection(this.db, 'Stall', stallId, 'OrdersRecieved'))
       return incomingOrders
     }
+  
+    // find the document based on user ID
+    async getOrdersByUserID(stallId, userId) {
+
+      // Create a field search against a collection.
+      const q = query(collection(this.db, "Stall", stallId, "OrdersRecieved"), where("UserID", "==", userId));
+      console.log(q)
+      return q
+    }
 
     // add orders into preparing orders collection (Orders Preparing)
     async acceptOrders(stallId, orderId, menuId, data) {
-      const addOrders = await setDoc(doc(this.db, 'Stall', stallId, 'OrdersRecieved', orderId, "orderList", menuId), data)
+      const addOrders = await setDoc(doc(this.db, 'Stall', stallId, 'OrdersPreparing', orderId, "orderList", menuId), data)
       return addOrders
     }
 
     // add orders into preparing orders collection (Orders Completed)
     async completedOrders(stallId, orderId, menuId, data) {
-      const addOrders = await setDoc(doc(this.db, 'Stall', stallId, 'OrdersRecieved', orderId, "orderList", menuId), data)
+      const addOrders = await setDoc(doc(this.db, 'Stall', stallId, 'OrdersCompleted', orderId, "orderList", menuId), data)
       return addOrders
     }
 
--- src/app/status/status.page.ts
+++ src/app/status/status.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { getFirestore } from '@angular/fire/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { OrderService } from '../services/order.service';
 
 @Component({
   selector: 'app-status',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatusPage implements OnInit {
 
-  constructor() { }
+  auth = getAuth();
+  userId: any;
+  stallId = [];
+
+  userOrders = [];
+
+  db = getFirestore();
+
+  constructor(
+    private orderService: OrderService,
+  ) { }
 
   ngOnInit() {
+    this.getCurrentUser();
+    this.getUserOrders();
+  }
+
+  getCurrentUser = async function () {
+    onAuthStateChanged(this.auth, async (user) => {
+      if (user) {
+        let users = this.auth.currentUser
+        this.userId = users.uid
+        console.log(this.userId)
+        this.getUserOrders()        
+      } else {
+        console.log("User is signed out")
+        this.navCntrl.navigateForward('splash');
+      }
+    });
+  };
+
+  getUserOrders(){
+    this.orderService.getAllStallId().then((res) => {
+      res.forEach((doc) =>{
+        this.stallId.push(doc.id)
+        console.log(this.stallId)
+      })
+    }).then(() => {
+      this.stallId.forEach((doc) => {
+        console.log(doc)
+        this.orderService.getOrdersByUserID(doc.id, this.userId).then((res) => {
+          console.log(res)
+          this.userOrders.push(res)
+        })
+      })
+    })
+    
   }
 
 }
