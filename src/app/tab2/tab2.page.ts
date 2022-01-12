353db52bb954974977d09d05c021eba61178f291
piers <piers1231@gmail.com>
2022-01-12 19:33:05 +0800

    image upload working

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,6 +8,9 @@ import { getAuth, onAuthStateChanged, reload } from 'firebase/auth';
 import { OrderService } from '../services/order.service';
 import { doc, getDoc } from 'firebase/firestore';
 import { getFirestore } from '@angular/fire/firestore';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { of } from 'rxjs';
+
 
 @Component({
   selector: 'app-tab2',
@@ -124,7 +127,7 @@ export class Tab2Page {
     const ableToGetData = await getDoc(doc(this.db, "User", users.uid))
     // console.log(ableToGetData.data())
     // console.log(this.stallId)
-    this.userDetails = { "UserID": this.userId, "UserFirstName": ableToGetData.data().firstName, "UserLastName": ableToGetData.data().lastName, "UserPhoneNumber": ableToGetData.data().phoneNumber, "Status": "Pending", "TotalPrice": this.grandtotal, "stallID": this.stallId}
+    this.userDetails = { "UserID": this.userId, "UserFirstName": ableToGetData.data().firstName, "UserLastName": ableToGetData.data().lastName, "UserPhoneNumber": ableToGetData.data().phoneNumber, "Status": "Pending", "TotalPrice": this.grandtotal, "stallID": this.stallId }
     // console.log(this.userId)
     this.orderService.addOrderId(this.stallId, this.userDetails).then((res) => {
       console.log(res.id)
@@ -164,7 +167,7 @@ export class Tab2Page {
   grandTotal() {
     // call first function and pass in a callback function which
     // first function runs when it has completed
-    this.calFood(function() {
+    this.calFood(function () {
       console.log("Test calFood Function");
     });
 
@@ -173,6 +176,79 @@ export class Tab2Page {
     console.log("Container Cost : $" + this.containercost)
     console.log("Surcharge : $" + this.surcharge)
     console.log("Grand Total : $" + this.grandtotal)
+  }
+
+  imageData: any
+
+  metadata = {
+    contentType: 'image/jpeg'
+  };
+
+  // Create a root reference
+  storage = getStorage();
+
+  // Create a reference to 'mountains.jpg'
+  storageRef = ref(this.storage);
+
+  // Create a reference to 'images/mountains.jpg'
+  mountainImagesRef = ref(this.storage, 'images/mountains.jpg');
+
+  // uploadTask = uploadBytesResumable(this.storageRef, this.imageData, this.metadata);
+
+  fileImg : any;
+
+  onchange = (img) => {
+    console.log("test meow")
+    const file = img.target.files
+    this.fileImg = file[0]
+    console.log(this.fileImg)
+    console.log(this.fileImg.name)
+
+  }
+  addImageToDatabase() {
+    const storage = getStorage();
+    const storageRef = ref(storage, `images/${this.fileImg.name}`);
+
+    const uploadTask = uploadBytesResumable(storageRef, this.fileImg);
+    console.log(uploadTask)
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // do shit here for the "progress bar"
+        // make global variable that updates then display it on HTML
+        console.log(snapshot)
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        console.log(error)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          // This is the file url for the image btw 
+          // Go add this to the SRC on front-end
+          console.log('File available at', downloadURL);
+        });
+      }
+    );
+
+    // // Pause the upload
+    // uploadTask.pause();
+
+    // // Resume the upload
+    // uploadTask.resume();
+
+    // // Cancel the upload
+    // uploadTask.cancel();
   }
 
 }
