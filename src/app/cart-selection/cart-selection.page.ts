ff91c8c990cd6f3b0b852930bfd9131080ecf6a6
timneam <timothyneam@gmail.com>
2022-01-27 22:37:21 +0800

    Able to display stall image in cart selection

--- src/app/cart-selection/cart-selection.page.ts
+++ src/app/cart-selection/cart-selection.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { CartService } from '../services/cart.service';
 import { UsersService } from '../services/users.service';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getDoc, doc, getDocs, collection, getFirestore } from 'firebase/firestore';
 
 @Component({
   selector: 'app-cart-selection',
@@ -13,8 +14,11 @@ import { UsersService } from '../services/users.service';
 export class CartSelectionPage implements OnInit {
 
   auth = getAuth();
+  db = getFirestore();
+  
   userId: any;
   stallsCart = [];
+  stallDetails = [];
 
   constructor(private cartService: CartService,
     private router: Router,
@@ -37,7 +41,7 @@ export class CartSelectionPage implements OnInit {
     });
   };
 
-  getStallsCart() {
+  async getStallsCart() {
     this.cartService.getAllStallsCart(this.userId).then(res => {
       if (res.length !== 0) {
         res.forEach(res => {
@@ -45,6 +49,18 @@ export class CartSelectionPage implements OnInit {
           this.stallsCart.push(stallData)
         });
       }
+    }).then(() => {
+      this.stallsCart.forEach(async (res) => {
+        const stallDetails = await getDoc(doc(this.db, 'Stall', res.id));
+        let stallInfo = {
+          'id': res.id,
+          'stallStatus': stallDetails.data().stallStatus,
+          'stallName': stallDetails.data().stallName,
+          'stallImg': stallDetails.data().stallImg
+        }
+        this.stallDetails.push(stallInfo)
+        console.log(this.stallDetails)
+      })
     });
   }
 
