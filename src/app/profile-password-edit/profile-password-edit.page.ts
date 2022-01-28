dc676a53792a8d1069f77c36a3a062489c90d91f
Chloe <chloeom@uuzu.com>
2022-01-28 11:24:15 +0800

    profile edit validator

--- src/app/profile-password-edit/profile-password-edit.page.ts
+++ src/app/profile-password-edit/profile-password-edit.page.ts
@@ -3,7 +3,7 @@ import { doc, getDoc, getFirestore } from 'firebase/firestore';
 import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { getAuth } from 'firebase/auth';
 import { UsersService } from '../services/users.service';
-
+import { AlertController, ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-profile-password-edit',
   templateUrl: './profile-password-edit.page.html',
@@ -16,15 +16,29 @@ export class ProfilePasswordEditPage implements OnInit {
   userData = null;
 
   constructor(private formBuilder: FormBuilder,
-    private userService: UsersService) { }
+    private userService: UsersService, private toastCtrl: ToastController,) { }
 
   ngOnInit() {
     this.updateUserPasswordForm = new FormGroup({
-      password: new FormControl()
-    })
+      password: new FormControl(),
+      reEnterPassword: new FormControl(), })
   }
 
   updateUserPassword(){
+    if(this.updateUserPasswordForm.value.password != this.updateUserPasswordForm.value.reEnterPassword){
+      return  this.toastCtrl.create({
+        message: 'Password fields do not match',
+        duration: 2000,
+        position: 'bottom'
+      }).then(alert=> alert.present()); 
+          }
+          else if(this.updateUserPasswordForm.value.password.length < 6) {
+            return  this.toastCtrl.create({
+              message: 'Password needs to be at least 6 characters long',
+              duration: 2000,
+              position: 'bottom'
+            }).then(alert=> alert.present()); 
+            }
     this.userService.updateUserPassword(
       this.updateUserPasswordForm.value.password,
     )
